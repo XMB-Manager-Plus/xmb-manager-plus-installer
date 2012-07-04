ffb48eb689e84a1820b600d6fa49dcf6e7eea052
andreus.sebes@gmail.com <andreus.sebes@gmail.com>
2012-07-04 19:28:15 +0100

    changed order of the menus

--- source/main.cpp
+++ source/main.cpp
@@ -18,10 +18,10 @@ std::string mainfolder="/dev_hdd0/game/XMBMANPLS/USRDIR";
 //std::string mainfolder="/dev_hdd0/game/XMBMANPLS/USRDIR/resources";
 std::string fw_version="";
 int fw_version_index=-1;
-std::string menu1[10][10];
-std::string menu1_fwv[10];
-int menu1_size[10];
-int menu2_size = 3;
+int menu1_size = 4;
+std::string menu2[10][10];
+std::string menu2_fwv[10];
+int menu2_size[10];
 
 static int exitapp, xmbopen;
 
@@ -172,18 +172,15 @@ s32 center_text_x(NoRSX *Graphics, int fsize, const char* message)
 
 s32 draw_menu(NoRSX *Graphics, int menu_id, int selected,int choosed, std::string status)
 {
-	std::string menu2[]    ={
+	std::string menu1[]    ={
 						"INSTALL XMB Manager Plus",
 						"INSTALL Rebug Package Manager",
-						"RESTORE Install Package Files"
+						"RESTORE a backup",
+						"Exit to XMB"
 						};
 
 	std::string IMAGE_PATH=mainfolder+"/data/images/xmbm_transparent.png";
-	//std::string TITLEFONT_PATH=mainfolder+"/data/fonts/GOODTIME.ttf";
-	//std::string TITLEFONT_PATH=mainfolder+"/data/fonts/arial.ttf";
-	//std::string TEXTFONT_PATH=mainfolder+"/data/fonts/arial.ttf";
-	//std::string TEXTFONTBOLD_PATH=mainfolder+"/data/fonts/arialbd.ttf";
-	int cury=0;
+	int posy=0;
 
 	Background B1(Graphics);
     //i want to display a PNG. i need to define the structure pngData (jpgData if you need to load a JPG).
@@ -197,46 +194,38 @@ s32 draw_menu(NoRSX *Graphics, int menu_id, int selected,int choosed, std::strin
 	//Now you loaded the png into memory. you need to display it: This will draw the image at x = 200 and y = 300 on the buffer.
 	I1.AlphaDrawIMG(imgX,imgY,&png);
 	int sizeTitleFont = 30;
-	int sizeFont = 20;
-	//Font F1(0xd38900, sizeTitleFont, TITLEFONT_PATH.c_str(), Graphics);
-	//Font F2(TEXTFONT_PATH.c_str(), Graphics);
-	//Font F3(TEXTFONTBOLD_PATH.c_str(), Graphics);
+	int sizeFont = 25;
 	Font F1(LATIN2, Graphics);
 	Font F2(LATIN2, Graphics);
-	Font F3(LATIN2, Graphics);
 
+	posy=260;
 	if (menu_id==1)
 	{
-		F1.Printf(100,220,	0xd38900, sizeFont, "%d",menu1_size[0]);
-		F1.Printf(100,240,	0xd38900, sizeFont, "%d",menu1_size[1]);
-		F1.Printf(100,260,	0xd38900, sizeFont, "%d",fw_version_index);
-		F1.Printf(center_text_x(Graphics, sizeTitleFont, "CHOOSE A FIRMWARE"),220,	0xd38900, sizeTitleFont, "CHOOSE A FIRMWARE");
-		for(int j=0;j<menu1_size[fw_version_index];j++)
+		F1.Printf(center_text_x(Graphics, sizeTitleFont, "CHOOSE WHAT TO INSTALL"),220, 0xd38900, sizeTitleFont, "CHOOSE WHAT TO INSTALL");
+		for(int j=0;j<menu1_size;j++)
 		{
-			if (j==choosed) F3.Printf(center_text_x(Graphics, sizeFont, menu1[fw_version_index][j].c_str()),280+(j*(sizeFont+4)),COLOR_RED,sizeFont, "%s",menu1[fw_version_index][j].c_str());
-			else if (j==selected) F3.Printf(center_text_x(Graphics, sizeFont, menu1[fw_version_index][j].c_str()),280+(j*(sizeFont+4)),COLOR_YELLOW,sizeFont, "%s",menu1[fw_version_index][j].c_str());
-			else F2.Printf(center_text_x(Graphics, sizeFont, menu1[fw_version_index][j].c_str()),280+(j*(sizeFont+4)),COLOR_WHITE,sizeFont, "%s",menu1[fw_version_index][j].c_str());
+			if (j<menu1_size-1) posy=posy+sizeFont+4;
+			else posy=posy+(2*(sizeFont+4));
+			if (j==choosed) F2.Printf(center_text_x(Graphics, sizeFont, menu1[j].c_str()),posy,COLOR_RED,sizeFont, "%s",menu1[j].c_str());
+			else if (j==selected) F2.Printf(center_text_x(Graphics, sizeFont, menu1[j].c_str()),posy,COLOR_YELLOW,sizeFont, "%s",menu1[j].c_str());
+			else F2.Printf(center_text_x(Graphics, sizeFont, menu1[j].c_str()),posy,COLOR_WHITE,sizeFont, "%s",menu1[j].c_str());
 		}
-		F2.Printf(center_text_x(Graphics, sizeFont, "Press START to exit"),280+((menu1_size[fw_version_index]+1)*(sizeFont+4)),COLOR_WHITE,sizeFont, "Press START to exit");
-		cury=280+((menu1_size[fw_version_index]+2)*(sizeFont+4));
 	}
 	else if (menu_id==2)
 	{
-		F1.Printf(center_text_x(Graphics, sizeTitleFont, "CHOOSE WHAT TO INSTALL"),220, 0xd38900, sizeTitleFont, "CHOOSE WHAT TO INSTALL");
-		for(int j=0;j<menu2_size;j++)
+		F1.Printf(center_text_x(Graphics, sizeTitleFont, "CHOOSE A FIRMWARE"),220,	0xd38900, sizeTitleFont, "CHOOSE A FIRMWARE");
+		for(int j=0;j<menu2_size[fw_version_index];j++)
 		{
-			if (j==choosed) F3.Printf(center_text_x(Graphics, sizeFont, menu2[j].c_str()),280+(j*(sizeFont+4)),COLOR_RED,sizeFont, "%s",menu2[j].c_str());
-			else if (j==selected) F3.Printf(center_text_x(Graphics, sizeFont, menu2[j].c_str()),280+(j*(sizeFont+4)),COLOR_YELLOW,sizeFont, "%s",menu2[j].c_str());
-			else F2.Printf(center_text_x(Graphics, sizeFont, menu2[j].c_str()),280+(j*(sizeFont+4)),COLOR_WHITE,sizeFont, "%s",menu2[j].c_str());
+			if (j<menu2_size[fw_version_index]-1) posy=posy+sizeFont+4;
+			else posy=posy+(2*(sizeFont+4));
+			if (j==choosed) F2.Printf(center_text_x(Graphics, sizeFont, menu2[fw_version_index][j].c_str()),posy,COLOR_RED,sizeFont, "%s",menu2[fw_version_index][j].c_str());
+			else if (j==selected) F2.Printf(center_text_x(Graphics, sizeFont, menu2[fw_version_index][j].c_str()),posy,COLOR_YELLOW,sizeFont, "%s",menu2[fw_version_index][j].c_str());
+			else F2.Printf(center_text_x(Graphics, sizeFont, menu2[fw_version_index][j].c_str()),posy,COLOR_WHITE,sizeFont, "%s",menu2[fw_version_index][j].c_str());
 		}
-		F2.Printf(center_text_x(Graphics, sizeFont, "Press O to go back"),280+((menu2_size+1)*(sizeFont+4)),COLOR_WHITE,sizeFont, "Press O to go back");
-		F2.Printf(center_text_x(Graphics, sizeFont, "Press START to exit"),280+((menu2_size+2)*(sizeFont+4)),COLOR_WHITE,sizeFont, "Press START to exit");
-		cury=280+((menu2_size+3)*(sizeFont+4));
 	}
-
 	u32 textX =(Graphics->width/2)-230;
-	F2.Printf(textX,cury+30,0xc0c0c0,sizeFont,     "Firmware version: %s", fw_version.c_str());
-	F2.Printf(textX+300,cury+30,0xc0c0c0,sizeFont,     "Status: %s", status.c_str());
+	F2.Printf(textX,posy+2*(sizeFont+4),0xc0c0c0,sizeFont,     "Firmware version: %s", fw_version.c_str());
+	F2.Printf(textX+300,posy+2*(sizeFont+4),0xc0c0c0,sizeFont,     "Status: %s", status.c_str());
 	
 	Graphics->Flip();
 
@@ -252,7 +241,7 @@ s32 main(s32 argc, const char* argv[])
 	sysUtilRegisterCallback(SYSUTIL_EVENT_SLOT0, eventHandler, NULL);
 	std::string firmware_choice;
 	std::string app_choice;
-	std::string menu2_val[]={ "xmbmanpls", "pkgmanage", "original" };
+	std::string menu1_val[]={ "xmbmanpls", "pkgmanage", "original", "exit" };
 
 	std::string dfile;
 	std::string direct;
@@ -264,7 +253,7 @@ s32 main(s32 argc, const char* argv[])
 	int ifwv=0;
 	int ifw=0;
 	int ret=0;
-	int menu_position=0;
+	int menu1_position=0;
 	int menu2_position=0;
 
 	uint8_t platform_info[0x18];
@@ -287,7 +276,7 @@ s32 main(s32 argc, const char* argv[])
 		dfile = direct + "/" + dirp->d_name;
 		if ( strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0 && strcmp(dirp->d_name, "") != 0)
 		{
-			menu1_fwv[ifwv]=dirp->d_name;
+			menu2_fwv[ifwv]=dirp->d_name;
 			ifwv++;
 		}
 	}
@@ -300,7 +289,7 @@ s32 main(s32 argc, const char* argv[])
 	else if (fw==0x30150) fw_version="3.15";
 	for (int h=0; h<ifwv; h++)
 	{
-		if (fw_version==menu1_fwv[h]) fw_version_index=h;
+		if (fw_version==menu2_fwv[h]) fw_version_index=h;
 	}
 	if (fw_version=="" || fw_version_index==-1)
 	{
@@ -312,7 +301,7 @@ s32 main(s32 argc, const char* argv[])
 	for (int h=0; h<ifwv; h++)
 	{
 		ifw=0;
-		direct=mainfolder+"/resources/"+menu1_fwv[h];
+		direct=mainfolder+"/resources/"+menu2_fwv[h];
 		dp = opendir (direct.c_str());
 		if (dp == NULL) return 0;
 		while ( (dirp = readdir(dp) ) )
@@ -320,18 +309,21 @@ s32 main(s32 argc, const char* argv[])
 			dfile = direct + "/" + dirp->d_name;
 			if ( strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0 && strcmp(dirp->d_name, "") != 0)
 			{
-				menu1[h][ifw]=dirp->d_name;
+				menu2[h][ifw]=dirp->d_name;
 				ifw++;
 			}
 		}
 		closedir(dp);
-		menu1_size[h]= ifw;
+		menu2[h][ifw]="Back to main menu";
+		ifw++;
+		menu2_size[h]= ifw;
 	}
 
-	//Start first menu
-	menu_position=0;
+
+	//Start second menu
+	menu1_position=0;
 	menu_1:
-	draw_menu(Graphics,1,menu_position,-1,"Waiting");
+	draw_menu(Graphics,1,menu1_position,-1,"Waiting");
 	while (1)
 	{
 		ioPadGetInfo (&padinfo);
@@ -341,41 +333,49 @@ s32 main(s32 argc, const char* argv[])
 			if (padinfo.status[i])
 			{
 				ioPadGetData (i, &paddata);
-				if (paddata.BTN_START)
-				{
-					goto end;
-				}
+				//if (paddata.BTN_START) goto end;
 				if (paddata.BTN_DOWN || paddata.ANA_L_V == 0x00FF || paddata.ANA_R_V == 0x00FF)
 				{
-					if (menu_position<menu1_size[fw_version_index]-1)
+					if (menu1_position<menu1_size-1)
 					{
-						menu_position++;
+						menu1_position++;
 						goto menu_1;
 					}
 				}
 				if (paddata.BTN_UP || paddata.ANA_L_V == 0x0000 || paddata.ANA_R_V == 0x0000)
 				{
-					if (menu_position>0)
+					if (menu1_position>0)
 					{
-						menu_position--;
+						menu1_position--;
 						goto menu_1;
 					}
 				}
 				if (paddata.BTN_CROSS)
 				{
-					draw_menu(Graphics,1,-1,menu_position,"Waiting");
-					firmware_choice=menu1[fw_version_index][menu_position];
+					draw_menu(Graphics,1,-1,menu1_position,"Waiting");
 					sleep(0.05);
-					goto continue_to_menu2;
+					if (menu1_position<menu1_size-2)
+					{
+						app_choice=menu1_val[menu1_position];
+						goto continue_to_menu2;
+					}
+					else if (menu1_position<menu1_size-1)
+					{
+						//goto continue_to_menu3;
+						goto menu_1;
+					}
+					else if (menu1_position<menu1_size)
+					{
+						goto end;
+					}
 				}
 			}
 		}
-		
 		sysUtilCheckCallback();
 	}
 
-	//Start second menu
 	continue_to_menu2:
+	//Start first menu
 	menu2_position=0;
 	menu_2:
 	draw_menu(Graphics,2,menu2_position,-1,"Waiting");
@@ -388,17 +388,11 @@ s32 main(s32 argc, const char* argv[])
 			if (padinfo.status[i])
 			{
 				ioPadGetData (i, &paddata);
-				if (paddata.BTN_START)
-				{
-					goto end;
-				}
-				if (paddata.BTN_CIRCLE)
-				{
-					goto menu_1;
-				}
+				//if (paddata.BTN_START) goto end;
+				if (paddata.BTN_CIRCLE) goto menu_1;
 				if (paddata.BTN_DOWN || paddata.ANA_L_V == 0x00FF || paddata.ANA_R_V == 0x00FF)
 				{
-					if (menu2_position<menu2_size-1)
+					if (menu2_position<menu2_size[fw_version_index]-1)
 					{
 						menu2_position++;
 						goto menu_2;
@@ -414,31 +408,42 @@ s32 main(s32 argc, const char* argv[])
 				}
 				if (paddata.BTN_CROSS)
 				{
-					draw_menu(Graphics,2,-1,menu2_position,"Installing...");
-					app_choice=menu2_val[menu2_position];
-					sleep(0.05);
-					draw_menu(Graphics,2,menu2_position,-1,"Installing...");
-					ret=0;
-					ret=install(firmware_choice, app_choice);
-					if (ret == 0)
+					if (menu2_position<menu2_size[fw_version_index]-1)
 					{
-						Mess.Dialog(MSG_OK,"Installed!\nPress OK to reboot.");
-						//draw_menu(Graphics,2,-1,menu2_position,"Installed!");
-						//sleep(2);
-						//draw_menu(Graphics,2,-1,menu2_position,"Rebooting...");
-						//sleep(2);
-						goto end_with_reboot;
+						draw_menu(Graphics,2,-1,menu2_position,"Installing...");
+						firmware_choice=menu2[fw_version_index][menu2_position];
+						sleep(0.05);
+						draw_menu(Graphics,2,menu2_position,-1,"Installing...");
+						ret=0;
+						ret=install(firmware_choice, app_choice);
+						if (ret == 0)
+						{
+							Mess.Dialog(MSG_OK,"Installed!\nPress OK to reboot.");
+							//draw_menu(Graphics,2,-1,menu2_position,"Installed!");
+							//sleep(2);
+							//draw_menu(Graphics,2,-1,menu2_position,"Rebooting...");
+							//sleep(2);
+							goto end_with_reboot;
+						}
+						else
+						{
+							Mess.Dialog(MSG_ERROR,"Not installed!\nSome error occured.");
+							draw_menu(Graphics,2,-1,menu2_position,"Error!");
+						}
 					}
 					else
 					{
-						Mess.Dialog(MSG_ERROR,"Not installed!\nSome error occured.");
-						draw_menu(Graphics,2,-1,menu2_position,"Error!");
+						draw_menu(Graphics,2,-1,menu2_position,"Waiting");
+						sleep(0.05);
+						goto menu_1;
 					}
 				}
 			}
 		}
+		
 		sysUtilCheckCallback();
 	}
+
 
 	end_with_reboot:
 	{
